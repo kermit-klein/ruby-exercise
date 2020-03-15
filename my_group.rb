35820d6e4b2eb7225ed201fc9b1c87b8bc722845
allezz <aerbay@gmail.com>
2020-03-15 20:31:48 +0100

    initial commit

---
+++ my_group.rb
@@ -0,0 +1,6 @@
+my_group = Array.new
+person_1 = {name:"John",gender:"male",age:"23"}
+person_2 = {name:"Jan",gender:"male",age:"27"}
+person_3 = {name:"Jerry",gender:"male",age:"45"}
+
+my_group = [person_1,person_2,person_3]
