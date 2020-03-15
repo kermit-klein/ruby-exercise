47e680c66aa575e3c261f83f741449e690865184
allezz <aerbay@gmail.com>
2020-03-15 22:17:03 +0100

    add hello_world,operators

--- my_group.rb
+++ my_group.rb
@@ -1,6 +1,10 @@
-my_group = Array.new
-person_1 = {name:"John",gender:"male",age:"23"}
-person_2 = {name:"Jan",gender:"male",age:"27"}
-person_3 = {name:"Jerry",gender:"male",age:"45"}
+my_group = Array.new;
+person_1 = {name:"John",gender:"man",age:"23"};
+person_2 = {name:"Jan",gender:"man",age:"27"};
+person_3 = {name:"Jessica",gender:"woman",age:"45"};
 
-my_group = [person_1,person_2,person_3]
+my_group = [person_1,person_2,person_3];
+
+my_group.each do |person|
+    puts person[:name] +" is a "+person[:age]+" years old "+person[:gender];
+end
---
+++ operators.rb
@@ -0,0 +1,27 @@
+a=20;
+b=10;
+
+a+b
+a-b
+a*b
+a/b
+a%b
+a**b
+a == b
+a != b
+a > b
+a < b
+a >= b
+a <= b
+a <=> b  #comparison
+a === b
+#a.equal ? b
+c = a + b
+c += a
+c -= a
+c *= a
+c /= a
+c %= a
+c **= a
+1 && 0
+0 || 0
