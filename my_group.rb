fd0991937fff8989040ad888ebd3de69190672ca
allezz <aerbay@gmail.com>
2020-03-15 23:46:58 +0100

    Add Class

--- my_group.rb
+++ my_group.rb
@@ -1,10 +1,24 @@
-my_group = Array.new;
-person_1 = {name:"John",gender:"man",age:"23"};
-person_2 = {name:"Jan",gender:"man",age:"27"};
-person_3 = {name:"Jessica",gender:"woman",age:"45"};
+# my_group = Array.new;
+# person1 = {name:"John",gender:"man",age:"23"};
+# person2 = {name:"Jan",gender:"man",age:"27"};
+# person3 = {name:"Jessica",gender:"woman",age:"45"};
 
-my_group = [person_1,person_2,person_3];
+# my_group.each do |person|
+#     puts person[:name] +" is a "+person[:age]+" years old "+person[:gender];
+# end
 
-my_group.each do |person|
-    puts person[:name] +" is a "+person[:age]+" years old "+person[:gender];
+class Group
+    def addPersons
+        person1 = {name:"Matt",gender:"man",age:"23"};
+        person2 = {name:"Yanni",gender:"woman",age:"27"};
+        person3 = {name:"Ceri",gender:"woman",age:"45"};
+        my_group = [person1,person2,person3];
+    end
+    def outputGroup(group)
+        group.each do |person|
+            puts person[:name] +" is a "+person[:age]+" years old "+person[:gender];
+        end
 end
+end
+finalgroup = Group.new
+finalgroup.outputGroup(finalgroup.addPersons)
