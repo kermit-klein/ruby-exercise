7b8f780f14c1ae1be9e455d311449d0407553d3a
allezz <aerbay@gmail.com>
2020-03-16 20:50:42 +0100

    Add arrays.rb

---
+++ arrays.rb
@@ -0,0 +1,32 @@
+# arr = [1,3,5,7,8,11]
+# $number = 3
+
+# def compare_(arr)
+#     arr.each do |element|
+#         if element == $number
+#             puts "Number is in the array"
+#         end
+#     end
+# end
+
+# compare_(arr)
+
+##########################################
+
+# arr = ["b","a"]
+# p arr = arr.product(Array(1..3))
+# p arr.first.delete(arr.first.last)
+
+###array.delete()  returns deleted element
+
+# arr = ["b","a"]
+# p arr = arr.product([Array(1..3)])
+# p arr.first.delete(arr.first.last)
+
+arr = [["test","hello","world"],["example","mem"]]
+puts arr[1][0]
+
+arr = [15,7,18,5,12,8,5,1]
+arr.index(5)
+arr.index[5]
+arr[5]
